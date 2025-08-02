8f74bd4c2417d1324a4e0147e7093ace46012311
kangsiwoo <me@kangsiwoo.com>
2025-08-02 18:45:54 +0900

    setup: Project

---
+++ src/main/kotlin/com/ittae/be/IttaeBeApplication.kt
@@ -0,0 +1,11 @@
+package com.ittae.be
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IttaeBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<IttaeBeApplication>(*args)
+}
